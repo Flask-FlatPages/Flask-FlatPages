c9f121199fe4c27f60910601645c53a621d26c9b
Marcel Radischat <m.radischat@datalyze-solutions.com>
2015-05-26 11:07:10 +0200

    fixed install_requires for pathlib in py33

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ README = open(rel('README.rst')).read()
 INIT_PY = open(rel('flask_flatpages', '__init__.py')).read()
 VERSION = re.search("__version__ = '([^']+)'", INIT_PY).group(1)
 
-IS_PY3 = sys.version_info[0] == 3
+IS_PY34 = (sys.version_info[0] == 3 and sys.version_info[1] >= 4)
 install_requires = ['Flask>=0.8',
                     'PyYAML>=3.10',
                     'Markdown>=2.3.1'
                     ]
-if not IS_PY3:
+if not IS_PY34:
     install_requires.append('pathlib>=1.0.1'
 )
 
