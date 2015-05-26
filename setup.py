7d88371c7e966b530b18766f722e4dbf0e8f9db7
Marcel Radischat <m.radischat@datalyze-solutions.com>
2015-05-26 10:43:35 +0200

    only install pathlib in py2

--- setup.py
+++ setup.py
@@ -12,6 +12,14 @@ README = open(rel('README.rst')).read()
 INIT_PY = open(rel('flask_flatpages', '__init__.py')).read()
 VERSION = re.search("__version__ = '([^']+)'", INIT_PY).group(1)
 
+IS_PY3 = sys.version_info[0] == 3
+install_requires = ['Flask>=0.8',
+                    'PyYAML>=3.10',
+                    'Markdown>=2.3.1'
+                    ]
+if not IS_PY3:
+    install_requires.append('pathlib>=1.0.1'
+)
 
 setup(
     name='Flask-FlatPages',
@@ -27,12 +35,7 @@ setup(
     ],
     zip_safe=False,
     platforms='any',
-    install_requires=[
-        'Flask>=0.8',
-        'PyYAML>=3.10',
-        'Markdown>=2.3.1',
-        'pathlib>=1.0.1'
-    ],
+    install_requires=install_requires,
     tests_require=['Pygments>=1.6'],
     extras_require={
         'tests': ['Pygments>=1.6'],
